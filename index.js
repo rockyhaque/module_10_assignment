909ff6d7cfa08f8728ade44107c5635e613c58d7
Rocky Haque <rockyhaque99@gmail.com>
2024-02-06 20:53:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const multer  = require('multer');
 const fs = require('fs');
 const url = require('url');
-const multer  = require('multer');
 
 
 const server = http.createServer((req, res) => {
@@ -11,25 +11,25 @@ const server = http.createServer((req, res) => {
     console.log('Server is listening on port 5500');
 
     if (parsedUrl.pathname === '/') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('This is Home Page');
         res.end();
     } else if (parsedUrl.pathname === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('This is About Page');
         res.end();
     } else if (parsedUrl.pathname === '/contact') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('This is Contact Page');
         res.end();
     } else if (parsedUrl.pathname === '/file-write') {
         fs.writeFile('demo.txt', 'hello world', (err) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, {'Content-Type': 'text/html'});
                 res.write('Error writing to file');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.write('hello world written successfully');
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write('hello world showing successfully');
             }
             res.end();
         });
@@ -37,20 +37,20 @@ const server = http.createServer((req, res) => {
         const upload = multer({ dest: 'uploads/' }).single('file');
         upload(req, res, (err) => {
             if (err instanceof multer.MulterError) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.write('Multer error occurred');
+                res.writeHead(500, {'Content-Type': 'text/html'});
+                res.write('Multer error');
             } else if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.write('Unknown error occurred');
+                res.writeHead(500, {'Content-Type': 'text/html'});
+                res.write('error');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.writeHead(200, {'Content-Type': 'text/html'});
                 res.write('Uploaded successfully');
             }
             res.end();
         });
     } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found');
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('Not Found');
         res.end();
     }
 });
